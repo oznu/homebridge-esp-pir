2e3ad44b199552412c04f14ba12f08fad725a2c3
oznu <dev@oz.nu>
2019-10-27 15:33:30 +1100

    use wifi manager

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,13 +1,12 @@
 
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>              // Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>       // Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>            // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <ESP8266mDNS.h>
+#include <WebSocketsServer.h>
 #include <ArduinoJson.h>
 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <WebSocketsServer.h>
-#include <ESP8266mDNS.h>
-
-
-MDNSResponder mdns;
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 // Replace with your network credentials
@@ -51,6 +50,12 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
     }
     case WStype_BIN:
       Serial.printf("[%u] get binary length: %u\r\n", num, length);
+      break;
+    case WStype_PING:
+      // Serial.printf("[%u] Got Ping!\r\n", num);
+      break;
+    case WStype_PONG:
+      // Serial.printf("[%u] Got Pong!\r\n", num);
       break;
     default:
       Serial.printf("Invalid WStype [%d]\r\n", type);
@@ -110,6 +115,11 @@ void checkForMotion() {
 }
 
 void setup(void) {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // turn LED on at boot
+  digitalWrite(LED_BUILTIN, LOW);
+
   delay(1000);
 
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
@@ -117,15 +127,28 @@ void setup(void) {
   WiFi.mode(WIFI_STA);
   WiFi.hostname(accessoryName);
 
-  WiFi.begin(ssid, password);
-
   Serial.println("");
 
+  // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
+
+  // sets timeout until configuration portal gets turned off
+  wm.setTimeout(600);
+
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // first parameter is name of access point, second is the password
+  if (!wm.autoConnect(accessoryName, "password"))
+  {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+
+    // reset and try again
+    ESP.reset();
+    delay(5000);
   }
+
+  WiFi.hostname(accessoryName);
+
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
@@ -135,7 +158,7 @@ void setup(void) {
   // declare sensor as input
   pinMode(inputPin, INPUT);    
 
-  if (mdns.begin(accessoryName, WiFi.localIP())) {
+  if (MDNS.begin(accessoryName, WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
 
@@ -144,14 +167,18 @@ void setup(void) {
   Serial.println("Web socket server started on port 81");
 
   // Add service to mdns-sd
-  mdns.addService("oznu-platform", "tcp", 81);
-  mdns.addServiceTxt("oznu-platform", "tcp", "type", "pir");
-  mdns.addServiceTxt("oznu-platform", "tcp", "mac", WiFi.macAddress());
+  MDNS.addService("oznu-platform", "tcp", 81);
+  MDNS.addServiceTxt("oznu-platform", "tcp", "type", "pir");
+  MDNS.addServiceTxt("oznu-platform", "tcp", "mac", WiFi.macAddress());
 
   checkForMotion();
+
+  // turn LED off once ready
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop(void) {
   webSocket.loop();
   checkForMotion();
+  MDNS.update();
 }
